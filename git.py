bedaa2ad52899f2510f142a512a28ea34c4e2abe
unknown <ranaabdulrehmanali145@gmail.com>
2024-10-03 17:06:23 +0500

    Added remaining code

--- git.py
+++ git.py
@@ -9,3 +9,18 @@ def add(x, y):
 
 def subtract(x, y):
     return x - y
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Error! Division by zero."
+
+def calculator():
+    print("Select operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
